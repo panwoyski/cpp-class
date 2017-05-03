c6de9f83f098935836c09d26929a23cc95a3fdc4
woyski <jonak.t.o@gmail.com>
2017-05-03 16:30:25 +0200

    Skelly for tests done

---
+++ zvector/zuza_vector.cpp
@@ -0,0 +1,9 @@
+#include "zuza_vector.h"
+#include <iostream>
+
+namespace zuza {
+    vector::vector(const char * a) {
+        std::cout << a << std::endl;
+    }
+}
+
---
+++ zvector/zuza_vector.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace zuza {
+    class vector {
+    public:
+        vector(const char *); 
+    };
+}
+
